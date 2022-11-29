80e55a1d984444c2f69bc55dbc7da5c1f5877e77
Jonathan V <hxn.dev@gmail.com>
2022-11-29 20:46:49 +0100

    add: accounts table main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+    import { Datatable } from 'svelte-simple-datatables'
+
+    export let data
+    let rows
+
+    const settings = {
+        sortable: true,
+        pagination: true,
+        rowsPerPage: 10,
+        scrollY: false,
+        blocks: {
+            searchInput: false, 
+            paginationButtons: true,
+            paginationRowCount: true,
+        }
+    }
+
+    let isMounted = false
+    onMount(() => {
+        isMounted = true
+    })
+</script>
+
+<div class="container mx-auto my-8">
+    <h1 class="text-4xl">Chift accounts</h1>
+
+    {#if isMounted}
+        <div class="accounts mt-6">
+            <Datatable settings={settings} data={data.records} bind:dataRows={rows}>
+                <thead>
+                    <th data-key="avatar">Avatar</th>
+                    <th data-key="first_name">Name</th>
+                    <th data-key="email">Email</th>
+                    <th data-key="street">Street</th>
+                    <th data-key="zip">Zip Code</th>
+                    <th data-key="country">Code & Country</th>
+                </thead>
+                <tbody>
+                {#if rows}
+                    {#each $rows as row}
+                    <tr>
+                        {#if atob(row.avatar_128).includes('<?xml')}
+                            <td><img class="avatar mx-auto " src="data:image/svg+xml;base64,{row?.avatar_128}" alt="Avatar"/></td>
+                        {:else}
+                            <td><img class="avatar mx-auto " src="data:image/png;base64,{row?.avatar_128}" alt="Avatar"/></td>
+                        {/if}
+                        <td>{row?.name}</td>
+                        <td>{row?.email}</td>
+                        <td>{row?.street}</td>
+                        <td>{row?.zip}</td>
+                        <td>{row?.country_id}</td>
+                    </tr>
+                    {/each}
+                {/if}
+                </tbody>
+            </Datatable>
+        </div>
+    {/if}
+</div>
